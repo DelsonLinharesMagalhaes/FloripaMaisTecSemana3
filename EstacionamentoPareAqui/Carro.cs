907d0a514869268ef4f1be0e1cbb1c3f8b482efd
Delson Magalhaes <delson_magalhaes@estudante.sesisenai.org.br>
2023-02-27 11:57:56 -0300

    Cadastrar carrot

---
+++ EstacionamentoPareAqui/Carro.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstacionamentoPareAqui
+{
+    public class Carro
+    {
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set;}
+        public string Marca { get; set; }
+        //public list<ticket> listaTicket { get; set; }
+
+        public Carro(){
+
+        }
+        public Carro(string placa, string modelo, string cor, string marca){
+        Placa = placa;
+        Modelo = modelo;
+        Cor = cor;
+        Marca = marca; 
+        }
+
+        
+
+    }
+
+    
+    
+}
--- EstacionamentoPareAqui/Program.cs
+++ EstacionamentoPareAqui/Program.cs
@@ -1,1 +1,58 @@
 ﻿using EstacionamentoPareAqui;
+
+
+string opcao;
+            
+do
+{
+    Console.WriteLine("Bem Vindo ao Estacionamento Pare Aqui!");
+    Console.WriteLine("**************************************");
+    Console.WriteLine("Escolha uma opção");
+    Console.WriteLine("-----------------");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Entrada do veículo");                
+    Console.WriteLine("3 - Saída do veículo");
+    Console.WriteLine("4 - Consultar histórico");
+    Console.WriteLine("5 - Sair"); 
+                
+    opcao = Console.ReadLine();
+
+    if (opcao == "1"){
+        Console.WriteLine("Cadastrar veículo");
+        Carro novoCarro = CadastrarVeiculo();
+        break;
+
+    }else if (opcao == "2"){
+        Console.WriteLine("Entrada  do veículo");
+        Console.Read();
+        break;
+        // EntradaDoVeiculo();
+    }else if (opcao == "3"){
+        Console.WriteLine("Saída do veículo");
+        Console.Read();
+        break;
+        // SaidaDoVeiculo();
+    }else if (opcao == "4"){
+        Console.WriteLine("Historico");
+        Console.Read();
+        break;
+        //  Historico();
+    }else if (opcao == "5"){ break; 
+    }else {Console.WriteLine("Opção Invalida!");
+            Console.Read();
+            break;}
+
+}while (opcao != "5");
+
+Carro CadastrarVeiculo(){
+    Carro carro = new Carro();
+    Console.WriteLine("Digite a placa: ");
+    carro.Placa = Console.ReadLine();
+    Console.WriteLine("Digite o modelo: ");
+    carro.Modelo = Console.ReadLine();
+    Console.WriteLine("Digite a cor: ");
+    carro.Cor = Console.ReadLine();
+    Console.WriteLine("Digite a Marca: ");
+    carro.Marca = Console.ReadLine();
+    return carro;
+    }
